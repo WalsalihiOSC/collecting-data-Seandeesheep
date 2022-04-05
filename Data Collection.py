c8045407b97f71e00edfe53aab142ae27231a020
Seandeesheep <99300392+Seandeesheep@users.noreply.github.com>
2022-04-05 20:07:53 +1200

    Added enter_data method and Person class
    DataCollectGUI class: 
    - now initializes a data list allowing for data storage
    - updated the formatting of the code
    - changed the scopes of some attributes for easier callback/access
    - created a enter data method that binds to the enter data button
        - ensures all the fields are entered correctly before appending the inputted information
        - added an error label that tells the user which field is entered incorrectly and informs the user if the data has been collected
    Person class:
    - an object that compiles the info of a person: name, age, and phone availability
    Created a DataDisplay class which will be used to display the data.

--- Data Collection.py
+++ Data Collection.py
@@ -7,50 +7,87 @@ from tkinter import *
 # objects
 class DataCollectGUI:
     def __init__(self, parent):
+        # initialising personal data list
+        self.data = []
+
         # initialising constants for graphics
-        FONT = "Arial 12 bold"
-        BG1 = 'magenta'
-        BG2 = 'light blue'
+        self.FONT = "Arial 12 bold"
+        self.BG1 = 'magenta'
+        self.BG2 = 'light blue'
 
         # initialising frames
-        main = Frame(parent)
-        f1 = Frame(main, bg=BG1)
-        f2 = Frame(main, bg=BG2)
+        self.main = Frame(parent)
+        f1 = Frame(self.main, bg=self.BG1)
+        f2 = Frame(self.main, bg=self.BG2)
 
-        # initialising label widgets
-        collect_data_label = Label(main, text="Collecting Person Data", bg=BG1, font=FONT)
-        name_label = Label(main, text='First Name:', bg=BG2)
-        age_label = Label(main, text='Age:', bg=BG2)
-        phone_q_label = Label(main, text='Do you have a mobile phone?', bg=BG2)
-
-        # initialising entry widgets
-        name_entry = Entry(main, width=30)
-        age_entry = Entry(main, width=30)
-
-        # initialising button variables
+        # initialising widget variables
         self.yes_or_no_var = StringVar()
+        self.name_var = StringVar()
+        self.age_var = StringVar()
         self.yes_or_no_var.set('*')
 
+        # initialising label widgets
+        collect_data_label = Label(self.main, text="Collecting Person Data", bg=self.BG1, font=self.FONT)
+        name_label = Label(self.main, text='First Name:', bg=self.BG2)
+        age_label = Label(self.main, text='Age:', bg=self.BG2)
+        phone_q_label = Label(self.main, text='Do you have a mobile phone?', bg=self.BG2)
+
+        # initialising entry widgets
+        self.name_entry = Entry(self.main, width=30, textvariable=self.name_var)
+        self.age_entry = Entry(self.main, width=30, textvariable=self.age_var)
+
         # initialising button widgets
-        show_all_button = Button(main, text="Show All", pady=2, padx=5, font=FONT)
-        yes_option = Radiobutton(main, text='Yes', value='Yes', variable=self.yes_or_no_var, bg=BG2)
-        no_option = Radiobutton(main, text='No', value='No', variable=self.yes_or_no_var, bg=BG2)
-        enter_button = Button(main, text='Enter', pady=2, padx=5, font=FONT)
+        show_all_button = Button(self.main, text="Show All", pady=2, padx=5, font=self.FONT)
+        yes_option = Radiobutton(self.main, text='Yes', value='Yes', variable=self.yes_or_no_var, bg=self.BG2)
+        no_option = Radiobutton(self.main, text='No', value='No', variable=self.yes_or_no_var, bg=self.BG2)
+        enter_button = Button(self.main, text='Enter Data', pady=2, padx=5, font=self.FONT, command=self.enter_data)
 
         # widget grid
-        main.grid(row=0, column=0)
+        self.main.grid(row=0, column=0)
         f1.grid(row=0, column=0, columnspan=2, sticky=NSEW)
-        f2.grid(row=1, column=0, rowspan=5, columnspan=2, sticky=NSEW)
+        f2.grid(row=1, column=0, rowspan=6, columnspan=2, sticky=NSEW)
         collect_data_label.grid(row=0, column=0, pady=20, padx=40)
         show_all_button.grid(row=0, column=1, pady=20, padx=20, sticky=W)
         name_label.grid(row=1, column=0, pady=6, padx=40, sticky=W)
         age_label.grid(row=2, column=0, pady=6, padx=40, sticky=W)
-        name_entry.grid(row=1, column=1, pady=6, padx=10)
-        age_entry.grid(row=2, column=1, pady=6, padx=10)
+        self.name_entry.grid(row=1, column=1, pady=6, padx=10)
+        self.age_entry.grid(row=2, column=1, pady=6, padx=10)
         phone_q_label.grid(row=3, pady=6, padx=40, sticky=W)
-        yes_option.grid(row=3, pady=6, column=1, sticky=W)
-        no_option.grid(row=4, pady=6, column=1, sticky=W)
-        enter_button.grid(row=5, columnspan=2, pady=20)
+        yes_option.grid(row=3, column=1, pady=6, sticky=W)
+        no_option.grid(row=4, column=1, pady=6, sticky=W)
+        enter_button.grid(row=5, columnspan=2, pady=10)
+
+    def enter_data(self):
+        name = self.name_var.get()
+        age = self.age_var.get()
+        phone = self.yes_or_no_var.get()
+        error_label = Label(self.main, text='', bg=self.BG2)
+        error_label.grid(row=6, columnspan=2, pady=10)
+        if name.isalpha() and age.isnumeric() and phone != '*':
+            error_label.configure(text="Data entered successfully!")
+            self.data.append(Person(name, age, phone))
+            self.yes_or_no_var.set('*')
+            self.name_entry.delete(0, END)
+            self.age_entry.delete(0, END)
+        else:
+            if not name.isalpha():
+                error_label.configure(text="Please enter a valid name")
+            elif not age.isnumeric():
+                error_label.configure(text="Please enter a positive integer for age")
+            elif phone == '*':
+                error_label.configure(text="Please select an option for phone availability")
+
+
+class DataDisplay:
+    pass
+
+
+class Person:
+    def __init__(self, name, age, phone_availability):
+        self.name = name
+        self.age = age
+        self.phone = phone_availability
+
 
 # main routine
 root = Tk()
